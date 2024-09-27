abeed0c21f320634406e4fb907e38ad1da39edb6
Abhishek Agrawal <151749923+abhishek-agrawal-2002@users.noreply.github.com>
2024-09-27 12:51:51 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,6 +1,30 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+   // program2.js
+
+function decodeTheRing(str, pattern) {
+  // Check if lengths of the strings match
+  if (str.length !== pattern.length) {
+      return false;
+  }
+
+  // Iterate over the characters of the string
+  for (let i = 0; i < str.length; i++) {
+      // If the current character in the pattern is '*', skip the check
+      if (pattern[i] === '*') {
+          continue;
+      }
+      // If the current character in str and pattern don't match, return false
+      if (str[i] !== pattern[i]) {
+          return false;
+      }
+  }
+
+  // If the loop completes, it means the strings match the pattern
+  return true;
+}
+
+module.exports = decodeTheRing;
 
   };
   
