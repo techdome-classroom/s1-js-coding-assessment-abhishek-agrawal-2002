bbf7865ad0b6d4aea315511137655a932f3e1039
Abhishek Agrawal <151749923+abhishek-agrawal-2002@users.noreply.github.com>
2024-09-27 12:51:28 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,30 @@
 const getTotalIsles = function (grid) {
 
+// program2.js
 
-  // write your code here
+function decodeTheRing(str, pattern) {
+  // Check if lengths of the strings match
+  if (str.length !== pattern.length) {
+      return false;
+  }
+
+  // Iterate over the characters of the string
+  for (let i = 0; i < str.length; i++) {
+      // If the current character in the pattern is '*', skip the check
+      if (pattern[i] === '*') {
+          continue;
+      }
+      // If the current character in str and pattern don't match, return false
+      if (str[i] !== pattern[i]) {
+          return false;
+      }
+  }
+
+  // If the loop completes, it means the strings match the pattern
+  return true;
+}
+
+module.exports = decodeTheRing;
 
 };
 
